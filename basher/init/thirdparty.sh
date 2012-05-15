f659b74de63c3454615922c2eabf1a0ba39b4f4e
uncreative <uncreative@sea-mac-11577.internal.popcap.com>
2012-05-15 11:43:44 -0700

    add homebrew bash completion try to fix auto jump to only be setup in this way if brew is installed

--- basher/init/thirdparty.sh
+++ basher/init/thirdparty.sh
@@ -1,3 +1,7 @@
-if [ -f `brew --prefix`/etc/autojump ]; then
-  . `brew --prefix`/etc/autojump
+if command -v brew &>/dev/null
+        then
+            if [ -f `brew --prefix`/etc/autojump ]; then
+              . `brew --prefix`/etc/autojump
+            fi
 fi
+
