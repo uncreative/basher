39628e7ceefd8c461055ec6014f6eca829febeab
uncreative <ayelet.goldin@gmail.com>
2013-09-08 13:37:54 -0700

    change the way auto jump is triggered

--- basher/init/thirdparty.sh
+++ basher/init/thirdparty.sh
@@ -1,7 +1,1 @@
-if command -v brew &>/dev/null
-        then
-            if [ -f `brew --prefix`/etc/autojump ]; then
-              . `brew --prefix`/etc/autojump
-            fi
-fi
-
+[[ -s `brew --prefix`/etc/autojump.sh ]] && . `brew --prefix`/etc/autojump.sh
