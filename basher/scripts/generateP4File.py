6b1b243cb54cd5f7d59a20881c75dd4d3367fa1f
Ayelet Goldin <agoldin@popcap.com>
2014-05-05 19:25:59 -0700

    more options in generateP4file than in previous

--- basher/scripts/generateP4File.py
+++ basher/scripts/generateP4File.py
@@ -5,7 +5,7 @@ generate a .p4 file with P4CLIENT='correct-p4-client' for every client for given
 """
 __author__ = "Ayelet Goldin (ayelet.goldin@gmail.com)"
 __version__ = "0.1"
-__copyright__ = "Copyright (c) 2011-2011 Ayelet Goldin"
+__copyright__ = "Copyright (c) 2014-2014 Ayelet Goldin"
 __license__ = "This script is in the public domain, free from copyrights or restrictions"
 
 import logging
@@ -15,6 +15,14 @@ import subprocess
 import re
 import os
 import sys
+import pwd
+import socket
+
+def getUsername():
+    return pwd.getpwuid( os.getuid() )[ 0 ]
+
+def getHostname():
+    return socket.gethostname()
 # regular expression to match client name to root path from the output of p4 clients
 reClientNameToRootPath = re.compile("Client\s+(\S*)\s+\d{4}/\d{2}/\d{2}\s+root\s+([^']*)\s+'.*'")
 
@@ -75,7 +83,7 @@ def p4LineNeeded(dotp4path, varToSet, valToSet):
     return True
 
 
-def generateP4ConfigFiles(p4user, p4host, varsToSet):
+def generateP4ConfigFiles(p4user, p4host, dirToProcess, varsToSet):
     loglevel=logging.DEBUG
 
     p4files = []
@@ -99,18 +107,23 @@ def generateP4ConfigFiles(p4user, p4host, varsToSet):
             if clientPossiblyBelongsToThisHost(p4clientHost, p4host):
                 varsToSet['P4CLIENT'] = p4client
                 if not os.path.isdir(convertedClientRootPath):
-                    raise ClientSkipException("found potential client %s with host '%s' for path %s, but no matching path. Not writing this p4 file" % (p4client, p4host, convertedClientRootPath))
-                else:
-                    for varToSet, valToSet in varsToSet.items():
-                        p4Line = "%s=%s" % (varToSet, valToSet)
-                        dotp4path = os.path.join(convertedClientRootPath, ".p4")
-
+                    if dirToProcess is None:
+                        raise ClientSkipException("found potential client %s with host '%s' for path %s, but no matching path. Not writing this p4 file" % (p4client, p4host, convertedClientRootPath))
+                    continue
+                for varToSet, valToSet in varsToSet.items():
+                    p4Line = "%s=%s" % (varToSet, valToSet)
+                    dotp4path = os.path.join(convertedClientRootPath, ".p4")
+                    if dirToProcess is None:
                         if p4LineNeeded(dotp4path, varToSet, valToSet):
                             p4files.append(p4client)
                             with open(dotp4path, 'a+') as dotp4out:
                                 dotp4out.write(os.linesep + p4Line)
                         else:
                             logging.debug("client '%s' already has %s" % (p4client, p4Line))
+
+                    elif dirToProcess.startswith(convertedClientRootPath):
+                        print 'echo "%s" >> %s' % (p4Line, dotp4path)
+                        return [],[],[]
             else:
                 skipped.append(p4client)
                 logging.info("client %s host '%s' does not match" % (p4client, p4clientHost))
@@ -124,33 +137,31 @@ def readOptions(argv):
     """
     parse command line options
     """
+    defaultHostname = getHostname()
+    defaultUsername = getUsername()
+
     parser = OptionParser(usage=globals()['__doc__'], version=__version__,)
     parser.add_option("--debug", action="store_true", dest="debug", default=False, help="make even more noise")
     parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False, help="make lots of noise")
     parser.add_option("-q", "--quiet", action="store_false", dest="verbose", help="be vewwy quiet (I'm hunting wabbits)  [default]")
+    parser.add_option("-u", "--user", action="store", type="string", dest="p4user", default=defaultUsername, help="username [user running script]")
+    parser.add_option("-o", "--hostname", action="store", type="string", dest="p4host", default=defaultHostname, help="hostname [current hostname]")
 
-    try:
-        options, args = parser.parse_args(argv)
-        if len(args) < 2:  # must provide integer values and command
-            raise Exception("You must supply a p4user and p4host")
-        if len(args) > 2:  # must provide two arguments
-            raise Exception("Too many arguments (%d) %s. Provide a two arguments: p4user and p4host." % (len(args), ','.join("'%s'" % arg for arg in argv)))
-        p4user = args[0]
-        p4host = args[1]
+    p4dir = None
+    options, args = parser.parse_args(argv)
+    if len(args) > 1:
+        p4dir = args[1]
 
-        if options.verbose:
-            loglevel = logging.INFO
-        else:
-            loglevel = logging.WARNING
-        if options.debug:
-            loglevel = logging.DEBUG
+    if options.verbose:
+        loglevel = logging.INFO
+    else:
+        loglevel = logging.WARNING
+    if options.debug:
+        loglevel = logging.DEBUG
 
-        logging.basicConfig(level=loglevel)
+    logging.basicConfig(level=loglevel)
 
-    except Exception, e:
-        parser.error(e)
-
-    return p4user, p4host
+    return options.p4user, options.p4host, p4dir
 
 
 def main(argv=None):
@@ -158,19 +169,21 @@ def main(argv=None):
     if argv is None:
         argv = sys.argv[1:]
 
-    p4user, p4host = readOptions(argv)  # setup any options (verbose / warn or quit on bad input)
+    p4user, p4host, p4dir = readOptions(argv)  # setup any options (verbose / warn or quit on bad input)
 
     starttime = time.time()
     logging.info("generating %s files for user %s on host %s" % ('.p4', p4user, p4host))
     varsToSet = {}  # for example: {'P4USER': p4user, 'P4HOST': p4host}
-    p4files, skipped, skippedForPotentialHost = generateP4ConfigFiles(p4user, p4host, varsToSet)
-    logging.info("generated %s" % ', '.join(p4files))
-    logging.warn("skipped %s" % ', '.join(skipped))
-    logging.warn("skippedForPotentialHost %s", ', '.join(skippedForPotentialHost))
+    print p4user, p4host, p4dir, varsToSet
+    p4files, skipped, skippedForPotentialHost = generateP4ConfigFiles(p4user, p4host, p4dir, varsToSet)
+    if len(p4files) > 0: logging.info("generated %s" % ', '.join(p4files))
+    if len(skipped) > 0: logging.warn("skipped %s" % ', '.join(skipped))
+    if len(skippedForPotentialHost) > 0: logging.warn("skippedForPotentialHost %s", ', '.join(skippedForPotentialHost))
 
     logging.info("generated in %f seconds" % (time.time() - starttime))
     sys.exit(0)
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    main()
+    # sys.exit(main())
